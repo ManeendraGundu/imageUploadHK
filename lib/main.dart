d223032ee464a0455309cc92f9a78e79f81dca92
ManeendraGundu <maneendra045@gmail.com>
2024-05-15 03:04:00 -0400

    uplad to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,154 +1,19 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
-import 'dart:io';
 
-import 'package:camera/camera.dart';
-
-
-// Future<void> main() async {
-//   // Ensure that plugin services are initialized so that `availableCameras()`
-//   // can be called before `runApp()`
-//   WidgetsFlutterBinding.ensureInitialized();
-//
-//   // Obtain a list of the available cameras on the device.
-//   final cameras = await availableCameras();
-//
-//   // Get a specific camera from the list of available cameras.
-//   final firstCamera = cameras.first;
-//
-//   runApp(
-//     MaterialApp(
-//       theme: ThemeData.dark(),
-//       home: TakePictureScreen(
-//         // Pass the appropriate camera to the TakePictureScreen widget.
-//         camera: firstCamera,
-//       ),
-//     ),
-//   );
-// }
-//
-//
-// class TakePictureScreen extends StatefulWidget {
-//   const TakePictureScreen({
-//     super.key,
-//     required this.camera,
-//   });
-//
-//   final CameraDescription camera;
-//
-//   @override
-//   TakePictureScreenState createState() => TakePictureScreenState();
-// }
-//
-// class TakePictureScreenState extends State<TakePictureScreen> {
-//   late CameraController _controller;
-//   late Future<void> _initializeControllerFuture;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     // To display the current output from the Camera,
-//     // create a CameraController.
-//     _controller = CameraController(
-//       // Get a specific camera from the list of available cameras.
-//       widget.camera,
-//       // Define the resolution to use.
-//       ResolutionPreset.medium,
-//     );
-//
-//     // Next, initialize the controller. This returns a Future.
-//     _initializeControllerFuture = _controller.initialize();
-//   }
-//
-//   @override
-//   void dispose() {
-//     // Dispose of the controller when the widget is disposed.
-//     _controller.dispose();
-//     super.dispose();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: const Text('Take a picture')),
-//       // You must wait until the controller is initialized before displaying the
-//       // camera preview. Use a FutureBuilder to display a loading spinner until the
-//       // controller has finished initializing.
-//       body: FutureBuilder<void>(
-//         future: _initializeControllerFuture,
-//         builder: (context, snapshot) {
-//           if (snapshot.connectionState == ConnectionState.done) {
-//             // If the Future is complete, display the preview.
-//             return CameraPreview(_controller);
-//           } else {
-//             // Otherwise, display a loading indicator.
-//             return const Center(child: CircularProgressIndicator());
-//           }
-//         },
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         // Provide an onPressed callback.
-//         onPressed: () async {
-//           // Take the Picture in a try / catch block. If anything goes wrong,
-//           // catch the error.
-//           try {
-//             // Ensure that the camera is initialized.
-//             await _initializeControllerFuture;
-//
-//             // Attempt to take a picture and get the file `image`
-//             // where it was saved.
-//             final image = await _controller.takePicture();
-//             print("Image Path ======> ${image.path}");
-//             if (!context.mounted) return;
-//
-//             // If the picture was taken, display it on a new screen.
-//             await Navigator.of(context).push(
-//               MaterialPageRoute(
-//                 builder: (context) => DisplayPictureScreen(
-//                   // Pass the automatically generated path to
-//                   // the DisplayPictureScreen widget.
-//                   imagePath: image.path,
-//                 ),
-//               ),
-//             );
-//           } catch (e) {
-//             // If an error occurs, log the error to the console.
-//             print(e);
-//           }
-//         },
-//         child: const Icon(Icons.camera_alt),
-//       ),
-//     );
-//   }
-// }
-//
-// // A widget that displays the picture taken by the user.
-// class DisplayPictureScreen extends StatelessWidget {
-//   final String imagePath;
-//
-//   const DisplayPictureScreen({super.key, required this.imagePath});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: const Text('Display the Picture')),
-//       // The image is stored as a file on the device. Use the `Image.file`
-//       // constructor with the given path to display the image.
-//       body: Image.file(File(imagePath)),
-//     );
-//   }
-// }
-
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'firebase_options.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:http/http.dart' as http;
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
+
+
 
 class MyApp extends StatelessWidget {
   @override
@@ -166,7 +31,7 @@ class ImageUploadScreen extends StatefulWidget {
 }
 
 class _ImageUploadScreenState extends State<ImageUploadScreen> {
-  File? _imageFile;
+  File? imageFile;
 
   Future<void> _getImage() async {
     final picker = ImagePicker();
@@ -174,45 +39,70 @@ class _ImageUploadScreenState extends State<ImageUploadScreen> {
 
     setState(() {
       if (pickedFile != null) {
-        _imageFile = File(pickedFile.path);
+        imageFile = File(pickedFile.path);
       } else {
         print('No image selected.');
       }
     });
   }
 
-  Future<void> _uploadImage() async {
-    if (_imageFile == null) {
-      print('No image selected.');
-      return;
-    }
-
-    var headers = {
-      'Content-Type': 'image/jpeg',
-    };
-
-    var request = http.MultipartRequest(
-        'PUT',
-        Uri.parse(
-            'https://wbln3zcwk9.execute-api.us-east-1.amazonaws.com/testing/fileupload-cancepro-1/abc.jpg'));
-
-    request.files.add(http.MultipartFile(
-        'file',
-        _imageFile!.readAsBytes().asStream(),
-        _imageFile!.lengthSync(),
-        filename: 'abc.jpg'));
-
-    request.headers.addAll(headers);
-
-    http.StreamedResponse response = await request.send();
-
-    if (response.statusCode == 200) {
-      print(await response.stream.bytesToString());
-      print("Image sent successfully");
-    } else {
-      print(response.reasonPhrase);
+  Future<void> uploadImageToFirebase() async {
+    try {
+      FirebaseStorage storage = FirebaseStorage.instance;
+      Reference ref = storage.ref().child("images/${DateTime.now().toString()}");
+      var metadata = SettableMetadata(
+        contentType: "image/jpeg",
+      );
+      UploadTask uploadTask = ref.putFile(imageFile!, metadata);
+      await uploadTask.whenComplete(() => null);
+      String url = await ref.getDownloadURL();
+      print("Image uploaded to Firebase: $url");
+    } catch (e) {
+      print("Error uploading image to Firebase: $e");
     }
   }
+
+  // Future<void> _uploadImage() async{
+  //   // var imageFile = File(image!.path);
+  //   print("Image Name =====> ${imageFile!.path}");
+  //   String fileName = basename(imageFile!.path);
+  //   FirebaseStorage storage = FirebaseStorage.instance;
+  //   Reference ref =
+  //   storage.ref().child("Image-");
+  // }
+
+  // Future<void> _uploadImage() async {
+  //   if (_imageFile == null) {
+  //     print('No image selected.');
+  //     return;
+  //   }
+  //
+  //   var headers = {
+  //     'Content-Type': 'image/jpeg',
+  //   };
+  //
+  //   var request = http.MultipartRequest(
+  //       'PUT',
+  //       Uri.parse(
+  //           'https://wbln3zcwk9.execute-api.us-east-1.amazonaws.com/testing/fileupload-cancepro-1/abc.jpg'));
+  //
+  //   request.files.add(http.MultipartFile(
+  //       'file',
+  //       _imageFile!.readAsBytes().asStream(),
+  //       _imageFile!.lengthSync(),
+  //       filename: 'abc.jpg'));
+  //
+  //   request.headers.addAll(headers);
+  //
+  //   http.StreamedResponse response = await request.send();
+  //
+  //   if (response.statusCode == 200) {
+  //     print(await response.stream.bytesToString());
+  //     print("Image sent successfully");
+  //   } else {
+  //     print(response.reasonPhrase);
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -221,9 +111,9 @@ class _ImageUploadScreenState extends State<ImageUploadScreen> {
         title: Text('Image Upload Example'),
       ),
       body: Center(
-        child: _imageFile == null
+        child: imageFile == null
             ? Text('No image selected.')
-            : Image.file(_imageFile!),
+            : Image.file(imageFile!),
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
@@ -235,7 +125,7 @@ class _ImageUploadScreenState extends State<ImageUploadScreen> {
           ),
           SizedBox(height: 16),
           FloatingActionButton(
-            onPressed: _uploadImage,
+            onPressed: uploadImageToFirebase,
             tooltip: 'Upload Image',
             child: Icon(Icons.cloud_upload),
           ),
